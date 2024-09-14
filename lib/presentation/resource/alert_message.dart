7da8b7ba5e2e566dcb9ed32232fb5d10dbeeb351
sageHabitus <dunkey2020@gmail.com>
2024-09-14 23:31:22 +0900

    🎨 add alert_message

---
+++ lib/presentation/resource/alert_message.dart
@@ -0,0 +1,9 @@
+class AlertMessage {
+  static const String appTitle = '깃허브 사용자';
+  static const String errorFetchingUsers = '사용자 데이터를 불러오는 데 실패했습니다.';
+  static const String errorFetchingRepos = '저장소 데이터를 불러오는 데 실패했습니다.';
+  static const String avatarPlaceholder = '이미지를 불러올 수 없습니다.';
+  static const String noUsers = '사용자가 없습니다.';
+  static const String noRepos = '저장소가 없습니다.';
+  static const String dataConversionFailure = '데이터 변환에 실패했습니다.';
+}
